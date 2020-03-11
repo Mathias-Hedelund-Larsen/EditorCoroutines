24f1c1446a6493b39392b06d6f3e030f81dbc36a
MathDodo <mathias.h.larsen@jp.dk>
2020-03-11 10:28:17 +0100

    Coroutines implementation

---
+++ Assets/FoldergeistAssets/Editor/EditorCoroutine/EditorCoroutine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FoldergeistAssets
+{
+    namespace EditorCoroutines
+    {
+        namespace Internal
+        {
+            public class EditorCoroutine
+            {
+                public Action<bool> _OnUpdate;
+                public IEnumerator _Enumerator;
+                public List<IEnumerator> _History = new List<IEnumerator>();
+            }
+        }
+    }
+}
---
+++ Assets/FoldergeistAssets/Editor/EditorCoroutine/EditorCoroutinesManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using FoldergeistAssets.EditorCoroutines.Internal;
+
+namespace FoldergeistAssets
+{
+    namespace EditorCoroutines
+    {
+        public static class EditorCoroutinesManager
+        {
+            private static readonly List<EditorCoroutine> coroutines = new List<EditorCoroutine>();
+
+            public static void Execute(IEnumerator enumerator, System.Action<bool> OnUpdate = null)
+            {
+                if (coroutines.Count == 0)
+                {
+                    EditorApplication.update += Update;
+                }
+
+                var coroutine = new EditorCoroutine { _Enumerator = enumerator, _OnUpdate = OnUpdate };
+                coroutines.Add(coroutine);
+            }
+
+            private static void Update()
+            {
+                for (int i = 0; i < coroutines.Count; i++)
+                {
+                    var coroutine = coroutines[i];
+                    bool done = !coroutine._Enumerator.MoveNext();
+                    if (done)
+                    {
+                        if (coroutine._History.Count == 0)
+                        {
+                            coroutines.RemoveAt(i);
+                            i--;
+                        }
+                        else
+                        {
+                            done = false;
+                            coroutine._Enumerator = coroutine._History[coroutine._History.Count - 1];
+                            coroutine._History.RemoveAt(coroutine._History.Count - 1);
+                        }
+                    }
+                    else
+                    {
+                        if (coroutine._Enumerator.Current is IEnumerator)
+                        {
+                            coroutine._History.Add(coroutine._Enumerator);
+                            coroutine._Enumerator = (IEnumerator)coroutine._Enumerator.Current;
+                        }
+                    }
+
+                    coroutine._OnUpdate?.Invoke(done);
+                }
+
+                if (coroutines.Count == 0) EditorApplication.update -= Update;
+            }
+
+            public static void StopAll()
+            {
+                coroutines.Clear();
+                EditorApplication.update -= Update;
+            }
+        }
+    }
+}
